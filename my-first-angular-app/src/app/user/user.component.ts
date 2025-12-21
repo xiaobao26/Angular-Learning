42f484baaa4bcd6d854f31ef1e6957f59204f1fa
xiaobao26 <xiaobao.xue@outlook.com>
2025-12-21 12:13:04 +1100

    feat: learn Signal

--- my-first-angular-app/src/app/user/user.component.ts
+++ my-first-angular-app/src/app/user/user.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent { 
-  @Input() avatar!: string;
-  @Input() name!: string;
+export class UserComponent {
+  // Old version use Input
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
+  // Old version use function
   get imagePath() {
     return "/assets/users/" + this.avatar;
   }
+
+  // New version use input()
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // New version use computed()
+  // imagePath = computed(() => {
+  //   return "/assets/users/" + this.avatar();
+  // })
 
   onSelected() {
     console.log("coming soon ...");
