c9101b3d0f2078da239c13610a8ce9fe5ff87198
xiaobao26 <xiaobao.xue@outlook.com>
2026-01-06 15:25:38 +1100

    feat: add cancelAddNewTask logic

--- my-first-angular-app/src/app/tasks/tasks.component.ts
+++ my-first-angular-app/src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { DUMMY_TASKS } from '../core/mock-data/dummy-tasks';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -12,6 +13,7 @@ export class TasksComponent {
   @Input({ required: true}) userid!: string;
   @Input({ required: true}) name!: string
   tasks = DUMMY_TASKS;
+  isAddingTask = false;
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userid);
@@ -20,5 +22,13 @@ export class TasksComponent {
   handleTaskCompleted(id: string) {
     console.log("completed!");
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
